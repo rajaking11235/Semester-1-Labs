7a7361ff404715f8ce62d5617cfa07fc8b931ba3
Abrahim Nisar <rajaking11235@gmail.com>
2026-01-23 10:43:58 +0500

    Add files via upload

---
+++ LAB 11/TASK 5/Task 5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//declaring, intializing pointer and dispaling appropriate message	
+	int *ptr=0;
+	cout<<"This is a null pinter nad it hold the memory address "<<ptr<<endl;
+}
