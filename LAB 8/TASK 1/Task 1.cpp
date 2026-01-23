532cd40db6b6aed16119cb4a52f407660c322893
Abrahim Nisar <rajaking11235@gmail.com>
2026-01-23 10:17:57 +0500

    Add files via upload

---
+++ LAB 8/TASK 1/Task 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//declaring variables
+	int n,i;
+	cout<<"Enter the upper limit"<<endl;
+	cin>>n;
+//using for loop to print number according to limit set by user
+	for(i=1; i<=n; i++ )
+	{
+		cout<<i<<endl;
+	}
+}
