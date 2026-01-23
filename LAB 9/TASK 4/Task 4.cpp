581cc53ca33b17e31b037f3cc565c308f5d09ead
Abrahim Nisar <rajaking11235@gmail.com>
2026-01-23 10:25:12 +0500

    Add files via upload

---
+++ LAB 9/TASK 4/Task 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//declaring array and varable
+	int n;
+	int integers[5]={-1,-2,0,1,2};
+//displaying sie of array
+	cout<<"The size of array is:"<<sizeof(integers[5])<<endl;
+}
